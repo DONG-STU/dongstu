d009ec33b8c482e543acf1f2c30d3d2cf78a4a5b
dongstu <sts05147@naver.com>
2024-11-04 09:33:54 +0900

    feat: 1번 (제이든) 구현

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print("hi")
+def to_jaden_case(string):
+    return string.title()
+
+# 사용자 입력 받기
+user_input = input("문장을 입력하시오: ")
+jaden_cased = to_jaden_case(user_input)
+print("Jaden-Cased:", jaden_cased)
